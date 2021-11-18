22710f2e660019058e1fd1c80575ff12433a0013
gbrs <61385511+gbrs@users.noreply.github.com>
2021-11-18 11:28:49 +0300

    experimented with memory profiler

--- Probe2.py
+++ Probe2.py
@@ -1,5 +1,17 @@
+from memory_profiler import profile
+
+
+@profile
 def sm():
-    sm = 0
-    for i in range(1000000):
-        sm += i
-    return sm
+    # arr1 = pd.DataFrame(np.arange(0, 10**6).reshape(1000, 1000))
+    # arr2 = pd.DataFrame(np.arange(0, 10**6).reshape(1000, 1000))
+    
+    arr1 = pd.DataFrame(np.random.rand(1000, 1000))
+    arr2 = pd.DataFrame(np.random.rand(1000, 1000))
+    arr3 = arr1 + arr2
+    
+    
+import numpy as np
+import pandas as pd
+
+sm()
